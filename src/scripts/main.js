acb4df980badd797c7b6e594401aee6d7bfe4153
Anori <anorifrell@gmail.com>
2025-04-07 21:19:54 +0300

    list_of_employees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,13 +2,32 @@
 
 // write code here
 
-const list = document.querySelector('ul');
+// const list = document.querySelector('ul');
 
-const sortedList = Array.from(list.children).sort((item1, item2) => {
-  return (
-    Number(item2.dataset.salary.replaceAll('$', '').replaceAll(',', '')) -
-    Number(item1.dataset.salary.replaceAll('$', '').replaceAll(',', ''))
-  );
-});
+// const sortedList = Array.from(list.children).sort((item1, item2) => {
+//   return (
+//     Number(item2.dataset.salary.replaceAll('$', '').replaceAll(',', '')) -
+//     Number(item1.dataset.salary.replaceAll('$', '').replaceAll(',', ''))
+//   );
+// });
 
-sortedList.forEach((item) => list.appendChild(item));
+// sortedList.forEach((item) => list.appendChild(item));
+
+function sortList(employees) {
+  return employees.sort((item1, item2) => {
+    return (
+      Number(item2.dataset.salary.replaceAll('$', '').replaceAll(',', '')) -
+      Number(item1.dataset.salary.replaceAll('$', '').replaceAll(',', ''))
+    );
+  });
+}
+
+function getEmployees(sortedEmployees) {
+  const list = document.querySelector('ul');
+
+  sortedEmployees.forEach((employee) => {
+    list.appendChild(employee);
+  });
+}
+
+getEmployees(sortList([...document.querySelectorAll('li')]));
