243729b207d9e5aa4dd81782172cb3958ee9fb16
Anori <anorifrell@gmail.com>
2025-04-07 20:49:01 +0300

    employees

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+
+const list = document.querySelector('ul');
+
+const sortedItems = Array.from(list.children).sort((item1, item2) => {
+  return (
+    Number(item2.dataset.salary.replaceAll('$', '').replaceAll(',', '')) -
+    Number(item1.dataset.salary.replaceAll('$', '').replaceAll(',', ''))
+  );
+});
+
+sortedItems.forEach((item) => list.appendChild(item));
